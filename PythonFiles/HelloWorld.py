1150544f1b69138afcf946398fbf01388ecb9469
꧁༒ 𝓜𝓪𝓼𝓪𝓶𝓾𝓷𝓮 ༒꧂ <aetulabing@gmail.com>
2025-09-15 09:21:30 +0000

    initial commit

---
+++ PythonFiles/HelloWorld.py
@@ -0,0 +1,24 @@
+print("Hello, World!")
+
+message = "Hello, World!"
+print(message)
+
+hello = "Hello,"
+world = "World!"
+print(hello + " " + world)
+
+name = input("Name: ")
+print(f"Hello, {name}")
+
+print("Hello,","World!")
+
+print("Hello,", end = " ")
+print("World!")
+
+print("Hello,", end="World")
+print("!")
+
+print("Hello", "Hello,", sep="-", end=" ")
+print("World!")
+
+# print("Hello" * 2)
